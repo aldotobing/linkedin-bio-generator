14d6a97a776721cce46030d1dde8a03c2a423755
Aldo Tobing <aldo_tobing@hotmail.com>
2025-01-15 14:19:53 +0700

    Update layout and add foundational files for LinkedIn Bio Generator

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+js;
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="LinkedIn Bio Generator" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="stylesheet" href="/styles.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
