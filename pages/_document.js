b3e5356c7edecbfeec915ca842391c3ba3f42c57
Aldo Tobing <aldo_tobing@hotmail.com>
2025-01-16 15:17:22 +0700

    Remove language attribute from HTML tag in document

--- pages/_document.js
+++ pages/_document.js
@@ -2,15 +2,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head>
         <link
           rel="icon"
           href="https://aldotobing.github.io/assets/img/favicon.png"
         />
+
         <meta
           name="description"
           content="LinkedIn Bio Generator - Generate professional LinkedIn bios effortlessly."
+        />
+        <meta property="og:title" content="LinkedIn Bio Generator" />
+        <meta
+          property="og:description"
+          content="Generate professional LinkedIn bios effortlessly."
+        />
+        <meta
+          property="og:image"
+          content="https://aldotobing.github.io/assets/img/preview.svg"
+        />
+        <meta
+          property="og:url"
+          content="https://aldotobing.github.io/linkedin-bio-generator"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
